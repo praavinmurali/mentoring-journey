5a266be433632e6c367a4ed5e6d5ce656d5d0177
praavinmurali <praavinmurali@gmail.com>
2026-07-13 19:04:08 +0530

    chore: p1.5-day3 mern complited

---
+++ MERN-P1/day03-p1-5/Countdown+Timer/Countdown Timer/script.js
@@ -0,0 +1,24 @@
+const startCountDown = () => {
+  const timeInput = document.getElementById("timeInput").value;
+  const timerDisplay = document.getElementById("timer");
+  const timeRemaining = parseInt(timeInput);
+
+  if (isNaN(timeRemaining) || timeRemaining <= 0) {
+    timerDisplay.textContent = "Please enter a valid number";
+  }
+  //clear the previous count down text
+  timerDisplay.textContent = `Time Left ${timeRemaining} seconds`;
+  for (let i = timeRemaining; i >= 0; i--) {
+    (function (count) {
+      setTimeout(() => {
+        if (count > 0) {
+          timerDisplay.textContent = `Time Left: ${count} seconds`;
+        } else {
+          timerDisplay.textContent = "Times's up";
+        }
+      }, (timeRemaining - count) * 1000);
+    })(i);
+  }
+};
+
+document.getElementById("startBtn").addEventListener("click", startCountDown);
